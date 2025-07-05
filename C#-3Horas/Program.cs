8f38416952f328df05c27e009d60e64210182600
Lucas Levi <138832200+LucasL-Dev@users.noreply.github.com>
2025-07-05 02:43:36 -0300

    Initial commit

---
+++ C#-3Horas/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace C__3Horas
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            double Finalnumeros = 0;
+            Console.WriteLine("Digite um número: ");
+            double number01 = int.Parse(Console.ReadLine());
+            Console.WriteLine("Digite Outro Númro: ");
+            double number02 = int.Parse(Console.ReadLine());
+            Console.WriteLine("Digite a Operação desejada (+, -, /, *)");
+            string operacao = (Console.ReadLine());
+
+            switch (operacao)
+            {
+                case "+":
+                    Finalnumeros = number01 + number02;
+                    Console.WriteLine(Finalnumeros);
+                    break;
+                case "-":
+                    Finalnumeros = number01 - number02;
+                    Console.WriteLine(Finalnumeros);
+                    break;
+                case "*":
+                    Finalnumeros = number01 * number02;
+                    Console.WriteLine(Finalnumeros);
+                    break;
+                case "/":
+                    Finalnumeros = number01 / number02;
+                    Console.WriteLine(Finalnumeros);
+                    break;
+                default:
+                    Console.WriteLine("Dado Incorreto, seu burro");
+                    break;
+            }
+        }
+    }
+}
---
+++ C#-3Horas/Properties/AssemblyInfo.cs
@@ -0,0 +1,33 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// As informações gerais sobre um assembly são controladas por
+// conjunto de atributos. Altere estes valores de atributo para modificar as informações
+// associadas a um assembly.
+[assembly: AssemblyTitle("C#-3Horas")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("C#-3Horas")]
+[assembly: AssemblyCopyright("Copyright ©  2025")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Definir ComVisible como false torna os tipos neste assembly invisíveis
+// para componentes COM. Caso precise acessar um tipo neste assembly de
+// COM, defina o atributo ComVisible como true nesse tipo.
+[assembly: ComVisible(false)]
+
+// O GUID a seguir será destinado à ID de typelib se este projeto for exposto para COM
+[assembly: Guid("23e52d82-3028-437e-8eae-091aa5e7afca")]
+
+// As informações da versão de um assembly consistem nos quatro valores a seguir:
+//
+//      Versão Principal
+//      Versão Secundária 
+//      Número da Versão
+//      Revisão
+//
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
